2448e65553668e6919045d85a4521d9cc238b997
Defalt <34065549+DefaltBit@users.noreply.github.com>
2019-03-12 13:20:35 +0100

    Update cloudflare.bat

--- cloudflare.bat
+++ cloudflare.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFClVQBSKfEe/C7QV+u3H3+OEtlgPUfEDcYzU1KfWbeMf5VDgc5osxH8Xzp9CBRhXHg==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFClVQBSKfEe/C7QV+u3Hy++UqVkSRN4cdIDO3rKCIfIWpED8cPY=
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @ECHO off
 cls
 :start
@@ -61,7 +28,7 @@ ECHO.
 cls
 netsh int show int
 set ding=
-set /p ding= Welke interface wil je gebruiken?(Type volledige naam): 
+set /p ding= Which interface do you want to use?(Type full name): 
 set DNS1=1.1.1.1
 set DNS2=1.0.0.1
 set DNS3=2606:4700:4700::1111
@@ -81,7 +48,7 @@ goto end
 cls
 netsh int show int
 set ding=
-set /p ding= Welke interface wil je resetten?(type volledige naam): 
+set /p ding= Which interface would you like to put back to default?(type full name): 
 echo .
 echo Ipv4 instellingen resetten
 netsh int ipv4 set dnsservers "%ding%" source=dhcp
@@ -92,11 +59,10 @@ goto end
 :doei
 cls
 echo. 
-echo Oke, houdoe.
+echo Ok bye.
 echo.
 pause
 goto end
 :end
 ipconfig /flushdns
-echo Houdoe
 
